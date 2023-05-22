661bf5061ba15c24f315d4e27301334e1d0b17f4
DhanushRaj5297 <81673441+DhanushRaj5297@users.noreply.github.com>
2023-05-22 22:20:57 +0530

    Add files via upload

---
+++ Pong_Game/src/main/java/com/mycompany/pong_game/Pong_Game.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pong_game;
+
+/**
+ *
+ * @author dhanush
+ */
+import javax.swing.*;
+import java.awt.*;
+    
+public class Pong_Game extends JFrame {
+    Pong_Game(){
+        GamePanel panel=new GamePanel();
+        add(panel);
+        setTitle("Pong Game");
+        getContentPane().setBackground(Color.darkGray);
+        pack();
+        setResizable(false);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        Pong_Game g=new Pong_Game();
+    }
+}
+
